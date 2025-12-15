c3c1fff5828d11a5f50543bcd5c1efd95295b3f4
DucD0m <domupnorth@hotmail.com>
2025-12-15 11:25:56 -0500

    Exclusion coverage dans websocket_routes.py # pragma: no cover

--- routes/websocket_routes.py
+++ routes/websocket_routes.py
@@ -1,5 +1,8 @@
 from classes.WebsocketHelper import WebsocketHelper
 
+# # pragma: no cover -> On ne calcule pas certaines parties du code dans les tests de couverture. Les fonctions sont testées
+# dans les tests des classes Game et WebsocketHelper.
+
 def register_websocket_routes(app, game):
 
   @app.websocket("/ws")
@@ -12,25 +15,20 @@ def register_websocket_routes(app, game):
       user_id = request.cookies.get("id")
       ws.user_id = int(user_id) if user_id else None
 
-      await ws.send("YOU|" + player_symbol)
-      await wsh.broadcast(game.board_state())
+      await ws.send("YOU|" + player_symbol) # pragma: no cover
+      await wsh.broadcast(game.board_state()) # pragma: no cover
 
 
       try:
           while True:
               move = await ws.recv()
 
-              # On ne calcule pas cette partie du code dans les tests de couverture. Les fonctions sont testées
-              # dans les tests des classes Game et WebsocketHelper.
-
-              # pragma: no cover start
-
               # Vérifie si l'autre joueur est toujours connecté
-              if await wsh.check_disconnect(ws, player_symbol):
+              if await wsh.check_disconnect(ws, player_symbol): # pragma: no cover
                   continue
 
               # Validation du coup
-              idx = game.validate_move(player_symbol, move)
+              idx = game.validate_move(player_symbol, move) # pragma: no cover
               if idx is None:
                   continue
 
@@ -38,35 +36,35 @@ def register_websocket_routes(app, game):
               game.board[idx] = player_symbol
 
               # Vérifie si une condition gagnante est présente.
-              winner_symbol = game.check_winner(game.board)
+              winner_symbol = game.check_winner(game.board) # pragma: no cover
               if winner_symbol:
-                  await wsh.broadcast(game.board_state())
-                  await wsh.broadcast("WIN|" + winner_symbol)
+                  await wsh.broadcast(game.board_state()) # pragma: no cover
+                  await wsh.broadcast("WIN|" + winner_symbol) # pragma: no cover
 
-                  game.insert_game(winner_symbol)
+                  game.insert_game(winner_symbol) # pragma: no cover
 
                   # Ferme toutes les connections.
-                  await wsh.close_all_connections()
+                  await wsh.close_all_connections() # pragma: no cover
 
                   # Réinitialisation de la partie.
-                  game.reset_game()
+                  game.reset_game() # pragma: no cover
 
                   continue
 
               # Vérifie si la partie est nulle.
-              if game.board_full(game.board):
-                  await wsh.broadcast(game.board_state())
-                  await wsh.broadcast("DRAW")
+              if game.board_full(game.board): # pragma: no cover
+                  await wsh.broadcast(game.board_state()) # pragma: no cover
+                  await wsh.broadcast("DRAW") # pragma: no cover
 
-                  game.insert_game(0)
-                  await wsh.close_all_connections()
-                  game.reset_game()
+                  game.insert_game(0) # pragma: no cover
+                  await wsh.close_all_connections() # pragma: no cover
+                  game.reset_game() # pragma: no cover
                   continue
 
               # Changement du premier joueur à jouer pour la prochaine partie.
               game.current_player = "O" if game.current_player == "X" else "X"
-              await wsh.broadcast(game.board_state())
-              # pragma: no cover stop
+              await wsh.broadcast(game.board_state()) # pragma: no cover
+
 
       finally:
           # Nettoyage des connections.
