bf398d180df58e922a79ccd7709326af1a83c84b
DucD0m <domupnorth@hotmail.com>
2025-12-25 19:31:15 -0500

    Nettoyage du fichier UserDAO.py

--- classes/UserDAO.py
+++ classes/UserDAO.py
@@ -6,11 +6,11 @@ class UserDAO:
         self._ensure_schema()
 
     def _ensure_schema(self):
-        """Create the users table if it does not exist."""
+        """Assure que la table users existe avant les appels de fonctions."""
         with sqlite3.connect(self.db_path) as conn:
-            conn.row_factory = sqlite3.Row
-            cur = conn.cursor()
-            cur.execute("""
+            conn.row_factory = sqlite3.Row # Permet l'accès aux colonnes par leur nom.
+            cursor = conn.cursor()
+            cursor.execute("""
                 CREATE TABLE IF NOT EXISTS users (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     username TEXT UNIQUE NOT NULL,
@@ -22,30 +22,30 @@ class UserDAO:
         """ Insertion d'un utilisateur. Retourne le id en cas de réussite, 0 si le nom d'utilisateur existe déjà"""
         with sqlite3.connect(self.db_path) as conn:
             conn.row_factory = sqlite3.Row
-            cur = conn.cursor()
+            cursor = conn.cursor()
             try:
-                cur.execute("""
+                cursor.execute("""
                     INSERT INTO users (username, password)
                     VALUES (:username, :password)
                 """, {"username": username, "password": password})
-                return cur.lastrowid
+                return cursor.lastrowid
 
-            except sqlite3.IntegrityError:
+            except sqlite3.IntegrityError: # Si le nom d'utilisateur existe déjà.
                 return 0
 
     def get_user(self, username, password):
         """ Récupération d'un utilisateur avec son mot de passe """
         with sqlite3.connect(self.db_path) as conn:
             conn.row_factory = sqlite3.Row
-            cur = conn.cursor()
-            cur.execute("""
+            cursor = conn.cursor()
+            cursor.execute("""
                 SELECT id, username
                 FROM users
                 WHERE username = :username
                 AND password = :password
                 LIMIT 1
             """, {"username": username, "password": password})
-            row = cur.fetchone()
+            row = cursor.fetchone()
             if row:
                 return {"id": row["id"], "username": row["username"]}
             return None
